9460d27d48b367abf14b0194a67a6fd3f4c3f840
wshert1688 <wsh1688@163.com>
2013-07-19 14:12:29 +0800

    ...

--- ssh.js
+++ ssh.js
@@ -1,23 +1,28 @@
 ﻿var ssh2 = require('ssh2'),
     colors = require('./colors');
 
-var c = new ssh2();
 
-exports.putFiles = function (servers, localPath, remotePath, srcServer, srcPath) {
-
+exports.putLocalFiles = function (servers, username, password, localPath, remotePath) {
+    for (var i = 0; i < servers.length; i++) {
+        putFile(servers[i], username, password, localPath, remotePath);
+    }
+}
+exports.putRemoteFiles = function (servers,username,password, localPath, remotePath, srcServer, srcPath) {
+    getFile(srcServer, username, password, localPath, srcPath, function () {
+        putLocalFiles(servers, username, password, localPath, remotePath);
+    })
 };
 exports.exec = function (servers, username, password, command) {
-
     for (var i = 0; i < servers.length; i++) {
         connection(servers[i], username, password, function (c) {
             c.exec(command, function (err, stream) {
                 if (err) throw err;
                 stream.on('data', function (data, extended) {
                     console.log("command exec success".rainbow, command);
-                    console.log("%s ".yellow, new String(data));
+                    console.log(" ", (new String(data)).yellow);
                 });
                 stream.on('exit', function (code, signal) {
-                    c.end();
+                    c.end(); 
                 });
             });
         });
@@ -28,26 +33,28 @@ function putFile(server, username, password, localPath, remotePath) {
         c.sftp(function (err, sftp) {
             if (err) throw err;
             sftp.on('end', function () {
-                console.log('SFTP :: SFTP session closed');
+                //console.log('SFTP :: SFTP session closed'.rainbow);
             });
+            
             sftp.fastPut(localPath, remotePath, function (err) {
                 if (err) throw err;
-                console.log("%s %s -> %s put success", server, localPath, remotePath);
+                console.log("%s %s -> %s put success".yellow, server, localPath, remotePath);
+                sftp.end();
                 c.end();
-            });
-        });
+            }); 
+        });  
     })
 }
 function connection(server, username, password, cb) {
+    var c = new ssh2();
     c.on('ready', function () {
-        console.log('Connection :: ready '.rainbow, server);
+        //console.log('Connection :: ready '.rainbow, server);
         cb(c);
     });
     c.on('error', function (err) {
         console.log('Connection :: error :: %s'.red, err);
     });
     c.on('end', function () {
-        console.log('Connection :: end'.rainbow);
         console.log('-----------------------------------------------------------------'.rainbow);
     });
     c.connect({
@@ -66,6 +73,7 @@ function getFile(server, username, password, localPath, remotePath, cb) {
             });
             sftp.fastGet(remotePath, localPath, function (err) {
                 if (err) throw err;
+                sftp.end();
                 c.end();
                 cb();
             });
--- task.js
+++ task.js
@@ -3,3 +3,5 @@ ssh = require(rootpath + 'ssh');
 
 
 ssh.exec(["10.168.100.189"], "root", "autohome123", "uptime");
+
+ssh.putLocalFiles(["10.168.100.189"], "root", "autohome123", "/root/linux-3.9.4.tar.gz", "/root/linux-3.9.4.tar.gz");
